dd46eb697c15d11d5947f007736ff85de51012e6
Michiel Uitdehaag <michiel@muisit.nl>
2025-09-24 13:14:20 +0200

    Update to allow element-plus to build

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,8 @@ export default ({mode}) => {
         define: {
             // enable hydration mismatch details in production build
             __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
-            global: {}
+            'process.env': {},
+            '_vm._self._c': {},
           }
     });
 }
