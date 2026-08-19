df2920996db1b0d743b9c32cfad8feda76c68507
Michiel Uitdehaag <michiel@muisit.nl>
2026-08-19 16:10:42 +0200

    Updated statuslist interface

--- src/pages/sub/StatusList.vue
+++ src/pages/sub/StatusList.vue
@@ -8,6 +8,7 @@ const preset = "statuslist";
 const date = ref('');
 const index = ref('');
 const lst = ref('');
+const name = ref('');
 const value = ref('');
 const mask = ref('-1');
 const output = ref('');
@@ -20,44 +21,12 @@ onUnmounted(() => {
     }
 })
 
-async function reserve()
-{
-    add();
-    const url = store.url + '/api/index';
-    const request = {
-        expirationDate: date.value
-    };
-
-    const response = await fetch(url, {
-        method:'POST',
-        body: JSON.stringify(request),
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + store.token
-        }
-    }).then((r) => r.json()).catch((e) => { console.error(e); return null; });
-
-    if (response) {
-        if (response.index) {
-            index.value = response.index;
-        }
-        if (response.url) {
-            lst.value = response.url;
-            if (lst.value.startsWith(store.url)) {
-                lst.value = lst.value.substring(store.url.length + 1); // skip the url plus path delimiter
-            }
-        }
-        output.value = JSON.stringify(response, null, 4);
-        value.value = '0';
-    }
-}
-
 async function revoke()
 {
     add();
-    const url = store.url + '/api/revoke';
+    const url = store.url + '/api/sl/' + name.value + '/revoke';
     const request = {
-        list: store.url + '/' + lst.value,
+        list: store.url + '/sl/' + name.value + '/' + lst.value,
         index: index.value,
         status: parseInt(value.value) ? 'unrevoke' : 'revoke'
     };
@@ -80,9 +49,9 @@ async function revoke()
 async function setstatus()
 {
     add();
-    const url = store.url + '/api/status';
+    const url = store.url + '/api/sl/' + name.value + '/status';
     const request = {
-        list: store.url + '/' + lst.value,
+        list: store.url + '/sl/' + name.value + '/' + lst.value,
         index: index.value,
         status: parseInt(value.value),
         mask: parseInt(mask.value)
@@ -105,7 +74,7 @@ async function setstatus()
 async function getstatus()
 {
     add();
-    const url = store.url + '/api/status/' + lst.value + '/' + index.value;
+    const url = store.url + '/api/sl/' + name.value + '/status/' + lst.value + '/' + index.value;
 
     const response = await fetch(url, {
         method:'GET',
@@ -132,7 +101,7 @@ async function getstatus()
 async function credential()
 {
     add();
-    const response = await fetch(store.url + '/' + lst.value, {
+    const response = await fetch(store.url + '/sl/' + name.value + '/' + lst.value, {
         method:'GET',
     });
 
@@ -148,7 +117,8 @@ function add()
         index: index.value,
         value: value.value,
         mask: mask.value,
-        lst: lst.value
+        lst: lst.value,
+        name: lst.name
     };
     store.update(preset, store.agent, dataPackage);
 }
@@ -162,6 +132,7 @@ function selectPreset()
         index.value = item.index;
         lst.value = item.lst;
         mask.value = item.mask;
+        name.value = item.name;
     }
 }
 
@@ -175,6 +146,9 @@ import PresetHeader from '@/components/PresetHeader.vue';
             <el-collapse-item title="Details">
                 <el-form-item label="URL">
                     <el-input v-model="store.url" type="text" disabled/>
+                </el-form-item>
+                <el-form-item label="Name">
+                    <el-input v-model="name" type="text"/>
                 </el-form-item>
                 <el-form-item label="List">
                     <el-input v-model="lst" type="text"/>
@@ -194,12 +168,10 @@ import PresetHeader from '@/components/PresetHeader.vue';
             </el-collapse-item>
         </el-collapse>
         <el-form-item label="Actions">
-            <el-button @click="reserve">Reserve</el-button>
             <el-button @click="revoke">{{ parseInt(value) ? 'Unrevoke' : 'Revoke' }}</el-button>
             <el-button @click="setstatus">Set</el-button>
             <el-button @click="getstatus">Get</el-button>
             <el-button @click="credential">Credential</el-button>
-            <el-button @click="remove">Delete</el-button>
         </el-form-item>
         <el-form-item label="Status">
             <pre>{{ output }}</pre>
